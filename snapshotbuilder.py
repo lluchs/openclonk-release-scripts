95e079476a80b380f85c0bd7e4c1ca4b29b78ae7
Armin Burgmeier <armin@arbur.net>
2012-01-08 14:08:03 +0100

    Make snapshot builder build tarballs for linux snapshots

--- snapshotbuilder.py
+++ snapshotbuilder.py
@@ -1,6 +1,8 @@
 import os
 import time
 import zipfile
+import tarfile
+import StringIO
 
 import periodicbuilder
 import hg
@@ -43,24 +45,48 @@ class SnapshotBuilder(periodicbuilder.PeriodicBuilder):
 			date = time.strftime('%Y%m%d')
 			filename = 'openclonk-snapshot-%s-%s-%s' % (date, rev, arch)
 
-			zip_filename = os.path.join(directory, filename + '.zip')
-			z = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
+			# TODO: Add an archive class...
+			def archive_name(basename):
+				if 'win32' in arch:
+					return os.path.join(directory, basename + '.zip')
+				else:
+					return os.path.join(directory, basename + '.tar.bz2')
 
-			for filename, stream in contentiter.ContentIter():
-				z.writestr(filename, stream.read())
+			def open_archive(basename):
+				if 'win32' in arch:
+					return zipfile.ZipFile(archive_name(basename), 'w', zipfile.ZIP_DEFLATED)
+				else:
+					return tarfile.open(archive_name(basename), 'w:bz2')
 
-			arch_iter = architer.ArchIter(arch)
-			for filename, stream in arch_iter:
-				z.writestr(filename, stream.read())
-			uuid = arch_iter.uuid
-
-			z.close()
+			def add_to_archive(archive, filename, content):
+				if 'win32' in arch:
+					archive.writestr(filename, content)
+				else:
+					info = tarfile.TarInfo(filename)
+					if filename == 'clonk' or filename == 'c4group':
+						info.mode = 0755
+					else:
+						info.mode = 0644
+					info.mtime = time.time() # TODO: Should be the same for all files -- set once at the beginning
+					info.size = len(content) # TODO: Would be cool if this could be dedcude automatically when reading the fileobj in addfile...
+					archive.addfile(info, StringIO.StringIO(content))
 
 			try:
+				archive = open_archive(filename)
+				for name, stream in contentiter.ContentIter():
+					add_to_archive(archive, name, stream.read())
+
+				arch_iter = architer.ArchIter(arch)
+				for name, stream in arch_iter:
+					add_to_archive(archive, name, stream.read())
+				uuid = arch_iter.uuid
+				archive.close()
+
 				uploader = upload.Uploader(self.log)
-				uploader.nightly_file(zip_filename, uuid, rev, arch)
-				os.unlink(zip_filename)
+				uploader.nightly_file(archive_name(filename), uuid, rev, arch)
+				os.unlink(archive_name(filename))
 			except autobuild.AutobuildException as ex:
-				upload.nightly_upload(None, ex.uuid) # make an entry for "failed build"
+				uploader = upload.Uploader(self.log)
+				uploader.nightly_file(None, ex.uuid, rev, arch) # make an entry for "failed build"
 
 		os.rmdir(directory)
